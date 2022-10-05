fc0f142aaf8547d05dc37c9eabf17d1a71aa0dc2
frank.potter@rabobank.nl <frank.potter@rabobank.nl>
2022-10-05 23:43:26 +0200

    substitute cluster

---
+++ k8s/nginx.sh
@@ -0,0 +1,7 @@
+NAMESPACE=nginx      
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --create-namespace \
+  --namespace $NAMESPACE \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
+
+k apply -f k8s/
